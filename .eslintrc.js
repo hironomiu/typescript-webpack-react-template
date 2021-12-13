722d5ec67dd4251b576213cf6692607ef518d210
hironomiu <hironomiu@gmail.com>
2021-12-13 10:11:15 +0900

    commited

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,18 +6,26 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
-  plugins: ['jest'],
+  plugins: ['jest', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2021,
+    project: './tsconfig.json',
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:jest/recommended',
+  ],
   // 'plugin:prettier/recommended'],
   rules: {
     'prefer-const': 'error',
